e9c51784158a6dbe990c8034b0a1116c2b456c4b
Ahmed Mohamed <85976531+AhmedMohamedGaber@users.noreply.github.com>
2024-09-22 07:19:21 +0300

    Delete Test1eco directory

--- Test1eco/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Test1eco.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
