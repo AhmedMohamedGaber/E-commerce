db3729caffd463b52ff26775f66954d9f468349a
Ahmed Mohamed <85976531+AhmedMohamedGaber@users.noreply.github.com>
2024-09-22 07:19:12 +0300

    Delete Demo1Ecommerce directory

--- Demo1Ecommerce/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Demo1Ecommerce.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
